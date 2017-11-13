cd096c26ed6344603ad1368b9832f9579072c7a0
Tomer Weller <tomer.weller@gmail.com>
2017-11-12 19:03:41 -0800

    fix thread safety issue

--- services/horizon/internal/test/trades/main.go
+++ services/horizon/internal/test/trades/main.go
@@ -7,23 +7,19 @@ import (
 )
 
 //getTestAsset generates an issuer on the fly and creates a CreditAlphanum4 Asset with given code
-func getTestAsset(code string) xdr.Asset {
+func getTestAsset(code string, accountNumber byte) xdr.Asset {
 	var codeBytes [4]byte
 	copy(codeBytes[:], []byte(code))
-	ca4 := xdr.AssetAlphaNum4{Issuer: getTestAccount(), AssetCode: codeBytes}
+	ca4 := xdr.AssetAlphaNum4{Issuer: getTestAccount(accountNumber), AssetCode: codeBytes}
 	return xdr.Asset{Type: xdr.AssetTypeAssetTypeCreditAlphanum4, AlphaNum4: &ca4, AlphaNum12: nil}
 }
 
-var accCounter byte
-
 //Get generates and returns an account on the fly
-func getTestAccount() xdr.AccountId {
-	accCounter++
-	acc, _ := xdr.NewAccountId(xdr.PublicKeyTypePublicKeyTypeEd25519, xdr.Uint256{accCounter})
+func getTestAccount(accountNumber byte) xdr.AccountId {
+	accountNumber++
+	acc, _ := xdr.NewAccountId(xdr.PublicKeyTypePublicKeyTypeEd25519, xdr.Uint256{accountNumber})
 	return acc
 }
-
-var opCounter int64
 
 //ingestTestTrade mock ingests a trade
 func ingestTestTrade(
@@ -34,7 +30,8 @@ func ingestTestTrade(
 	buyer xdr.AccountId,
 	amountSold int64,
 	amountBought int64,
-	timestamp int64) error {
+	timestamp int64,
+	opCounter int64) error {
 
 	trade := xdr.ClaimOfferAtom{}
 	trade.AmountBought = xdr.Int64(amountBought)
@@ -49,13 +46,14 @@ func ingestTestTrade(
 
 //PopulateTestTrades generates and ingests trades between two assets according to given parameters
 func PopulateTestTrades(q *Q, startTs int64, numOfTrades int, delta int64) (err error, ass1 xdr.Asset, ass2 xdr.Asset) {
-	acc1 := getTestAccount()
-	acc2 := getTestAccount()
-	ass1 = getTestAsset("usd")
-	ass2 = getTestAsset("euro")
+	acc1 := getTestAccount(1)
+	acc2 := getTestAccount(2)
+	ass1 = getTestAsset("usd", 3)
+	ass2 = getTestAsset("euro", 4)
 
 	for i := 1; i <= numOfTrades; i++ {
-		err = ingestTestTrade(q, ass1, ass2, acc1, acc2, int64(i*100), int64(i*100)*int64(i), startTs+(delta*int64(i-1)))
+		err = ingestTestTrade(
+			q, ass1, ass2, acc1, acc2, int64(i*100), int64(i*100)*int64(i), startTs+(delta*int64(i-1)),int64(i))
 		//tt.Assert.NoError(err)
 		if err != nil {
 			return
